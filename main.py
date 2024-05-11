2cca2ac905858e120309c032774a8321c193ea52
innalytics34 <arun@innalytics.in>
2024-05-11 09:35:52 +0530

    initial commit

--- main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-
-app = FastAPI()
-
-
-@app.get('/')
-def read_root():
-    return {"message": "Hello, World! This is my Python API running on Azure App Service with FastAPI."}
